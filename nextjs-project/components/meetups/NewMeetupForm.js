88c84ef2b97a000bdcfa3b666150e377f0307e3e
loretari <loretari@gmail.com>
2023-05-17 15:16:53 +0300

    init: add the New Meetup Form

--- nextjs-project/components/meetups/NewMeetupForm.js
+++ nextjs-project/components/meetups/NewMeetupForm.js
@@ -1,49 +1,54 @@
-import classes from './NewMeetupForm.module.css';
-import Card from "../ui/Card";
-import { useRef} from 'react';
+import { useRef } from "react"
+
+import Card from "../ui/Card"
+import classes from "./NewMeetupForm.module.css"
 
 function NewMeetupForm(props) {
-    const titleInputRef = useRef();
-    const imageInputRef = useRef();
-    const addressInputRef = useRef();
-    const descriptionInputRef = useRef();
+    const titleInputRef = useRef()
+    const imageInputRef = useRef()
+    const addressInputRef = useRef()
+    const descriptionInputRef = useRef()
 
-function submitHandler(event) {
-    event.preventDefault();
-}
+    function submitHandler(event) {
+        event.preventDefault()
 
-const meetupData = {
-    title: enteredTitle,
-    image: enteredImage,
-    address: enteredAddress,
-    description: enteredDescription,
-};
+        const enteredTitle = titleInputRef.current.value
+        const enteredImage = imageInputRef.current.value
+        const enteredAddress = addressInputRef.current.value
+        const enteredDescription = descriptionInputRef.current.value
 
-props.onAddMeetup(meetupData);
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+        }
 
-
+        props.onAddMeetup(meetupData)
+    }
 
     return (
         <Card>
             <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
-                    <label htmlFor= 'title'>Meetup Title</label>
-                    <input type= 'text' required id = 'title' ref={titleInputRef}/>
+                    <label htmlFor='title'>Meetup Title</label>
+                    <input type='text' required id='title' ref={titleInputRef} />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor= 'image'>Meetup Image</label>
-                    <input type= 'url' required id = 'image' ref={imageInputRef}/>
+                    <label htmlFor='image'>Meetup Image</label>
+                    <input type='url' required id='image' ref={imageInputRef} />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor= 'address'>Address</label>
-                    <input type= 'text' required id = 'address' ref={addressInputRef}/>
+                    <label htmlFor='address'>Address</label>
+                    <input type='text' required id='address' ref={addressInputRef} />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor= 'description'>Description</label>
+                    <label htmlFor='description'>Description</label>
                     <textarea
-                    id= 'description'
-                    required rows= '5'
-                    ref={descriptionInputRef}
+                        id='description'
+                        required
+                        rows='5'
+                        ref={descriptionInputRef}
                     ></textarea>
                 </div>
                 <div className={classes.actions}>
@@ -54,4 +59,4 @@ props.onAddMeetup(meetupData);
     )
 }
 
-export default NewMeetupForm;
+export default NewMeetupForm
