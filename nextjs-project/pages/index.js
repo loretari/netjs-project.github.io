470ea398ae33e7bf12fe8dde0df1b38bfaeea632
loretari <loretari@gmail.com>
2023-05-18 08:59:06 +0300

    init: add Custom Components

--- nextjs-project/pages/index.js
+++ nextjs-project/pages/index.js
@@ -6,14 +6,14 @@ const DUMMY_MEETUPS = [
     {
         id: 'm1',
         title: "A First Meetup",
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
         address: 'Some address 5, 12345 Some city',
         description: 'This is a first meetup!'
     },
     {
         id: 'm2',
         title: "A Second Meetup",
-        image: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
         address: 'Some address 10, 12345 Some city',
         description: 'This is a second meetup!'
     }
