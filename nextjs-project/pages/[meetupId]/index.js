8aa9c7c7fcb65109f2537a695d3b17dbf4fc0448
loretari <loretari@gmail.com>
2023-05-18 10:28:23 +0300

    init: add Params for SSG Data Fetching

--- nextjs-project/pages/[meetupId]/index.js
+++ nextjs-project/pages/[meetupId]/index.js
@@ -13,4 +13,40 @@ return (
 )
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: "m1",
+                },
+            },
+            {
+                params: {
+                    meetupId: "m2",
+                },
+            },
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    // fetch data for a single meetup
+
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+
+    return {
+        props: {
+            image:'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
+            id: meetupId,
+            title: 'First Meetup',
+            address:'Some address 5, 12345 Some city',
+            description:'This is a first meetup!',
+        }
+
+    }
+}
+
 export default MeetupDetails;
--- nextjs-project/pages/index.js
+++ nextjs-project/pages/index.js
@@ -28,13 +28,28 @@ function HomePage(props) {
 
 
 }
+//
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+//
+//
+//     // fetch data from an API
+//
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
 
 export async function getStaticProps() {
     // fetch data from an API
     return {
         props: {
             meetups: DUMMY_MEETUPS
-        }
+        },
+        revalidate: 1
     };
 }
 export default HomePage;
